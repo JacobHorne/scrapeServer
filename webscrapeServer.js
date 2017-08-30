9df68f94d2e659d1fc67cb14eca6734d25e7a855
Jake <jacobhorne@gmail.com>
2017-08-30 15:47:08 -0400

    First Commit

---
+++ webscrapeServer.js
@@ -0,0 +1,41 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var path = require('path'); 
+var app = express();
+
+
+var assetsPath = path.join(__dirname, '../static');
+app.use(express.static(assetsPath));
+
+//cors//////////////////////////////////////////////////////
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/scrape', function (req, res) {
+	let url = req.query.url; //grab parameter
+    let tag = req.query.tag; //grab second parameter
+	request(url, function(error, response, html) {
+        if (!error){
+            $ = cheerio.load(html);
+            res.send($(tag).text());
+        } else {
+            res.send("Something when wrong");
+        }
+    });
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
