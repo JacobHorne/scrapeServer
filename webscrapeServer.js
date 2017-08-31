7a9b13c620f062c14433063e0413d4e57bd8e5a0
Jake <jacobhorne@gmail.com>
2017-08-31 18:34:44 -0400

    index

--- webscrapeServer.js
+++ webscrapeServer.js
@@ -12,14 +12,14 @@ var app = express();
 app.use('/', express.static(path.join(__dirname, 'dist')))
 
 //cors////////////////////////////////////////////////////
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/scrape', function (req, res) {
