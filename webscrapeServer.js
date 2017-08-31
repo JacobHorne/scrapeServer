aca21b502ca55c9298ce5568e201db04b548f142
Jake <jacobhorne@gmail.com>
2017-08-31 17:26:01 -0400

    new build files added

--- webscrapeServer.js
+++ webscrapeServer.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/scrape', function (req, res) {
